be1124cc0d592dca9b7399c95c293b4d0570c3ab
Jannik Fromme <jannik.fromme@yahoo.de>
2021-05-24 11:57:48 -0500

    authentication

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyDv4hocTo4gdY0CFxX4Udldntk8dQIoMhk",
+  authDomain: "final-project---nba-stats.firebaseapp.com",
+  projectId: "final-project---nba-stats",
+  storageBucket: "final-project---nba-stats.appspot.com",
+  messagingSenderId: "108245735768",
+  appId: "1:108245735768:web:6c86aadac2fa1de428ad2d"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
