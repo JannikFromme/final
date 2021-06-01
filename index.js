fda201f97e148573fd6e2f4f1b3acf0564692603
Jannik Fromme <jannik.fromme@yahoo.de>
2021-06-01 12:36:58 -0500

    Notes box

--- index.js
+++ index.js
@@ -65,6 +65,31 @@ firebase.auth().onAuthStateChanged(async function(user) {
           let json2 = await response2.json()
           console.log(json2)
           console.log(json2.league.standard.stats.careerSummary.apg)
+
+          //get reference to notes section
+          let notes = document.querySelector(`#notes`)
+
+          //insert html for notes section
+          notes.innerHTML =`
+          <form class="bg-gray-900 opacity-75 w-1/2 shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <label class="block text-blue-300 py-2 font-bold mb-2">
+                Your Notes
+              </label>
+            <textarea class="form-textarea mt-1 block w-full overflow-y-scroll h-32" 
+              placeholder="Record your thoughts on ${playerFirst} ${playerLast}."></textarea>
+            </div>
+
+            <div class="flex items-center justify-between pt-4">
+              <button
+                class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="button"
+              >
+                Save notes
+              </button>
+            </div>
+          </form>
+          `
         }
       }
       // - Get a reference to the element containing the user-entered year
